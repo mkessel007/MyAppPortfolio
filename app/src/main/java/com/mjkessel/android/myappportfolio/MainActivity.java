07f1d73c225abc730c9fe3e8b440000220b81de8
Michael Kessel <mjkessel@gmail.com>
2015-08-04 00:43:55 -0700

    Initial working project release

--- app/src/main/java/com/mjkessel/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/mjkessel/android/myappportfolio/MainActivity.java
@@ -1,18 +1,99 @@
 package com.mjkessel.android.myappportfolio;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnSpotify).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onClickToast(v);
+            }
+        });
+
+        findViewById(R.id.btnScoreapp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onClickToast(v);
+            }
+        });
+
+        findViewById(R.id.btnLibraryapp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onClickToast(v);
+            }
+        });
+
+        findViewById(R.id.btnBuildbigger).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onClickToast(v);
+            }
+        });
+
+        findViewById(R.id.btnXyzreader).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onClickToast(v);
+            }
+        });
+
+        findViewById(R.id.btnCapstonemyapp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onClickToast(v);
+            }
+        });
     }
 
+    private void onClickToast(View v) {
+        switch (v.getId()) {
+            case R.id.btnSpotify:
+                Toast.makeText(this, "This button will launch my Spotify Streamer app!", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.btnScoreapp:
+                Toast.makeText(this, "This button will launch my Scores app!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.btnLibraryapp:
+                Toast.makeText(this, "This button will launch my Library app!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.btnBuildbigger:
+                Toast.makeText(this, "This button will launch my Build it Bigger app!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.btnXyzreader:
+                Toast.makeText(this, "This button will launch my XYZ Reader app!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.btnCapstonemyapp:
+                Toast.makeText(this, "This button will launch my capstone app!", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                break;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,4 +116,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
